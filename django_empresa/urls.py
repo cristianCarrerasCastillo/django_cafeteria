f8fca5ff334ea6bf66ded7b3a1eb8d458d297bd5
Cristian Carreras <cristian.carreras@smartmining.biz>
2021-04-07 00:49:24 -0400

    se correlaciono las rutas de blog, contact y services en el url del proyecto, llamandolos como include. se termino de correlacionar las pag html con la base.html

--- django_empresa/urls.py
+++ django_empresa/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from core import views as core_views
 
 urlpatterns = [
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('', include('blog.urls')),
+    path('', include('services.urls')),
     path('admin/', admin.site.urls),
 ]
