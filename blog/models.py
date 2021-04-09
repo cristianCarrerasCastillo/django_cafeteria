b7e99a632a0dfcaf3e34c36d701028320f09626d
Cristian Carreras <Cristian.carrerascastillo@gmail.com>
2021-04-09 00:29:46 -0400

    Creacion del formulario de la aplicacion blog, ahora esta aplicacion lee la base de datos para obtener los usuarios registrados y las categorias creadas para tagear los post,

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    edited = models.DateTimeField(auto_now=True, verbose_name="Fecha Edición")
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    published = models.DateTimeField(verbose_name="Fecha de Publicación", default=now)
+    image = models.ImageField(verbose_name="Imagen", upload_to="blog", null = True, blank = True)
+    author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.PROTECT)
+    categories = models.ManyToManyField(Category, verbose_name="Categorias")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    edited = models.DateTimeField(auto_now=True, verbose_name="Fecha Edición")
+
+    class Meta:
+        verbose_name = "Entrada"
+        verbose_name_plural = "Entradas"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
