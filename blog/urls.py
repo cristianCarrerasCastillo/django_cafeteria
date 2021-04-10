082f01dc622e7a4cb52dfe27f560081d3bfaa2d0
Cristian Carreras <Cristian.carrerascastillo@gmail.com>
2021-04-09 21:45:34 -0400

    se crea la funcion post en la app blog, esta con error, por lo cual se debe corregir para el correcto funcionamiento

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.blog, name='blog'),
+    path('', views.blog, name='blog'),
+    path('category/<int:category_id>/', views.category, name="category"),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post, Category
 
 # Create your views here.
 def blog(request):
-    return render(request, "blog/blog.html")
+    posts = Post.objects.all()
+    return render(request, "blog/blog.html", {'posts':posts})
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, "blog/category.html", {"category":category})
+
